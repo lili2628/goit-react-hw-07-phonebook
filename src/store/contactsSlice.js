63f764dc4cfb63e1aa2f4f9b1223b4a6cf331fb5
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-06-03 00:21:35 +0300

    update

---
+++ src/store/contactsSlice.js
@@ -0,0 +1,55 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './operations';
+
+const onPending = state => {
+  state.isLoading = true;
+};
+
+const onRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const contactsSlice = createSlice({
+    name: 'contacts',
+    initialState: {
+        contacts: [],
+        isLoading: false,
+        error: null,
+    },  
+    extraReducers: {
+        [fetchContacts.pending]: onPending,
+        [fetchContacts.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.contacts = action.payload;
+        },
+        [fetchContacts.rejected]: onRejected,
+
+        [addContact.pending]: onPending,
+        [addContact.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.contacts = [
+                {
+                name: action.payload.name,
+                phone: action.payload.phone,
+                },
+                ...state.contacts];
+        },
+        [addContact.rejected]: onRejected,
+
+        [deleteContact.pending]: onPending,
+        [deleteContact.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.contacts = state.contacts.filter(contact => contact.id !== action.payload.id);
+        },
+        [deleteContact.rejected]: onRejected,
+    },
+});
+
+export const contactReducer = contactsSlice.reducer;
+
+
+ 
